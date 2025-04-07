1226a36345152147ff26a0a2ef0d5a7b358e3f7b
qgelmaus <qgelmaus@gmail.com>
2025-04-07 10:59:19 +0200

    removed transactions and transfers for now

--- src/graphql/schema/index.ts
+++ src/graphql/schema/index.ts
@@ -5,8 +5,7 @@ import gql from "graphql-tag";
 import { mergeTypeDefs, mergeResolvers } from "@graphql-tools/merge";
 
 import * as accounts from "./accounts";
-import * as transactions from "./transactions";
-import * as transfers from "./transfers";
+
 import { DateScalar } from "../scalar/scalarResolver";
 
 const baseSchema = fs.readFileSync(
@@ -17,25 +16,10 @@ const accountSchema = fs.readFileSync(
 	path.join(__dirname, "./accounts/typeDefs.gql"),
 	"utf8",
 );
-const transactionSchema = fs.readFileSync(
-	path.join(__dirname, "./transactions/typeDefs.gql"),
-	"utf8",
-);
-const transferSchema = fs.readFileSync(
-	path.join(__dirname, "./transfers/typeDefs.gql"),
-	"utf8",
-);
 
-export const typeDefs = mergeTypeDefs([
-	gql(baseSchema),
-	gql(accountSchema),
-	gql(transactionSchema),
-	gql(transferSchema),
-]);
+export const typeDefs = mergeTypeDefs([gql(baseSchema), gql(accountSchema)]);
 
 export const resolvers = mergeResolvers([
 	{ Date: DateScalar },
 	accounts.resolvers,
-	transactions.resolvers,
-	transfers.resolvers,
 ]);
