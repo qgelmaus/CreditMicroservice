3470855a9ddb2c4024014735a0fc78fb7e2b2642
qgelmaus <qgelmaus@gmail.com>
2025-04-11 14:01:46 +0200

    removed test client from client.ts

--- src/creditaccounts/infrastructure/db/client.ts
+++ src/creditaccounts/infrastructure/db/client.ts
@@ -1,7 +1,9 @@
-import { PrismaClient as ProdClient } from "@prisma/client";
-import { PrismaClient as TestClient } from ".prisma/test-client";
+import { PrismaClient } from "@prisma/client";
 
-const isTest = process.env.NODE_ENV === "test";
-const client = isTest ? new TestClient() : new ProdClient();
-
-export const prisma: ProdClient = client as ProdClient;
+export const prisma = new PrismaClient({
+	datasources: {
+		db: {
+			url: process.env.DATABASE_URL,
+		},
+	},
+});
