7b07247003c486b08cbc84a8638b86df95b9f3b5
qgelmaus <qgelmaus@gmail.com>
2025-05-07 21:12:40 +0200

    Removed geist

--- apps/customerdemo/src/app/layout.tsx
+++ apps/customerdemo/src/app/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({
 	variable: "--font-inter",
-	subsets: ["latin"],
-});
-
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
 	subsets: ["latin"],
 });
 
